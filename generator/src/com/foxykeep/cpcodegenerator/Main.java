fb64fbae77b43ffae5e07d263e91b5e7a0cff26a
Foxykeep <foxykeep@gmail.com>
2013-02-09 14:46:58 -0800

    Update .gitignore

--- generator/src/com/foxykeep/cpcodegenerator/Main.java
+++ generator/src/com/foxykeep/cpcodegenerator/Main.java
@@ -1,5 +1,12 @@
 package com.foxykeep.cpcodegenerator;
 
+import com.foxykeep.cpcodegenerator.generator.DatabaseGenerator;
+import com.foxykeep.cpcodegenerator.model.TableData;
+import com.foxykeep.cpcodegenerator.util.PathUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -10,13 +17,6 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import com.foxykeep.cpcodegenerator.generator.DatabaseGenerator;
-import com.foxykeep.cpcodegenerator.model.TableData;
-import com.foxykeep.cpcodegenerator.util.PathUtils;
 
 public class Main {
 
